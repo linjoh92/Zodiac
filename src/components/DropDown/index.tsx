0f5a10cd32297b8ba59bf25293cd0084df189d32
Linn <linn.johansson2@edu.cmeducations.se>
2023-10-04 15:47:05 +0200

    Fix dropdown on mobile

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -27,7 +27,7 @@ export default function DropDown({ value, onChange }: DropDownProps) {
         </Menu.Button>
       </div>
       <Transition as={Fragment}>
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-xl bg-white shadow-lg ring-1 ring-baseText ring-opacity-5 focus:outline-none">
+        <Menu.Items className="mt-2 w-40 rounded-xl bg-white shadow-lg ring-1 ring-baseText ring-opacity-5 focus:outline-none">
           <div className="py-2">
             {ZodiacSigns.map((zodiac) => {
               return (
